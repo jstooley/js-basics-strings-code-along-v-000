436c5df0fbec2eb925d387b1728b87977dfe3024
Justin Tooley <jstooley823@gmail.com>
2018-02-13 21:57:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
 let currentUser =  'Justin';
 
-const welcomeMessage = 'Welcome to Flatbook, !';
+const welcomeMessage = 'Welcome to Flatbook, currentUser!';
 
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, !';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, currentUser!';
 
-const shortGreeting = 'Welcome, !';
+const shortGreeting = 'Welcome, currentUser!';
